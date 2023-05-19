f703fa2e9be938d83df968923f87df43a0c3b7a6
Salaheldin Mohamed <75949002+salah-mo@users.noreply.github.com>
2023-05-19 04:01:39 +0300

    Visualization Refactor

--- Data_Visualization.py
+++ Data_Visualization.py
@@ -3,12 +3,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
+# Method to load and display data from a csv file
 def load_merged_data():
     merged_data = pd.read_csv("merged_data.csv")
     merged_data["Date"] = pd.to_datetime(merged_data["Date"])
     return merged_data
 
 
+# 1. Make a chart to illustrate if weekly sales are increasing or decreasing over time.
 def plot_weekly_sales_trend(merged_data):
     weekly_sales = merged_data.groupby(merged_data["Date"].dt.strftime('%Y-%U'))["Weekly_Sales"].sum()
     plt.plot(weekly_sales)
@@ -18,6 +20,7 @@ def plot_weekly_sales_trend(merged_data):
     plt.show()
 
 
+# 2. Make a chart to show how much each brand sells.
 def plot_brand_sales(merged_data):
     brand_sales = merged_data.groupby("Store")["Weekly_Sales"].sum()
     plt.bar(brand_sales.index, brand_sales)
@@ -27,6 +30,7 @@ def plot_brand_sales(merged_data):
     plt.show()
 
 
+# 3. Determine the top ten selling stores.
 def plot_top_ten_stores(merged_data):
     store_sales = merged_data.groupby("Store")["Weekly_Sales"].sum()
     top_ten_stores = store_sales.sort_values(ascending=False).head(10)
@@ -37,6 +41,7 @@ def plot_top_ten_stores(merged_data):
     plt.show()
 
 
+# 4. Make a histogram to show the top 10 stores sales.
 def plot_top_ten_stores_histogram(merged_data):
     store_sales = merged_data.groupby("Store")["Weekly_Sales"].sum()
     top_ten_stores = store_sales.sort_values(ascending=False).head(10)
@@ -47,6 +52,7 @@ def plot_top_ten_stores_histogram(merged_data):
     plt.show()
 
 
+# 5. Create a chart that compares average weekly sales for the top ten selling stores during holidays and non-holidays.
 def plot_holiday_vs_non_holiday_sales(merged_data):
     merged_data['IsHolidayWeek'] = merged_data['Holiday'].apply(lambda x: 'Holiday' if x == True else 'Non-Holiday')
     store_sales = merged_data.groupby(['Store', 'IsHolidayWeek'])['Weekly_Sales'].mean().reset_index()
@@ -61,6 +67,7 @@ def plot_holiday_vs_non_holiday_sales(merged_data):
     plt.show()
 
 
+# 6. Create a chart that displays the average weekly sales for each brand department for the top 10 selling stores.
 def plot_brand_dept_sales(merged_data):
     top_stores = merged_data.groupby("Store")["Weekly_Sales"].sum().sort_values(ascending=False).head(10).index.tolist()
     top_stores_data = merged_data[merged_data["Store"].isin(top_stores)]
@@ -75,6 +82,7 @@ def plot_brand_dept_sales(merged_data):
     plt.show()
 
 
+# 7. Make a line chart to show the relationship between weekly sales and weather Temperature.
 def plot_sales_vs_temperature(merged_data):
     temp_sales = merged_data.groupby("Temperature")["Weekly_Sales"].mean()
     plt.plot(temp_sales)
@@ -84,6 +92,7 @@ def plot_sales_vs_temperature(merged_data):
     plt.show()
 
 
+# 8.	Make a line chart to show the relationship between the cost of fuel and weather weekly sales.
 def plot_sales_vs_temp_and_fuel(merged_data):
     temp_fuel_sales = merged_data.groupby(["Temperature", "Fuel_Price"])["Weekly_Sales"].mean().reset_index()
     plt.figure(figsize=(15, 10))
@@ -94,6 +103,7 @@ def plot_sales_vs_temp_and_fuel(merged_data):
     plt.show()
 
 
+# Main method to call all the methods
 def main():
     merged_data = load_merged_data()
     plot_weekly_sales_trend(merged_data)
@@ -106,5 +116,6 @@ def main():
     plot_sales_vs_temp_and_fuel(merged_data)
 
 
+# Call the main method to run the program
 if __name__ == "__main__":
     main()
